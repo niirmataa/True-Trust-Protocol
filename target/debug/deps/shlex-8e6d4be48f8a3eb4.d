047fb235717d79997775843a14e61cd1f20ff223
niirmataa <niirmata@tuta.io>
2025-11-20 03:58:44 +0100

    Add PQClean Falcon files

---
+++ target/debug/deps/shlex-8e6d4be48f8a3eb4.d
@@ -0,0 +1,8 @@
+C:\dev\tt-chain\TRUE-TRUST-PROTOCOL\target\debug\deps\shlex-8e6d4be48f8a3eb4.d: C:\Users\alicj\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\shlex-1.3.0\src\lib.rs C:\Users\alicj\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\shlex-1.3.0\src\bytes.rs
+
+C:\dev\tt-chain\TRUE-TRUST-PROTOCOL\target\debug\deps\libshlex-8e6d4be48f8a3eb4.rlib: C:\Users\alicj\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\shlex-1.3.0\src\lib.rs C:\Users\alicj\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\shlex-1.3.0\src\bytes.rs
+
+C:\dev\tt-chain\TRUE-TRUST-PROTOCOL\target\debug\deps\libshlex-8e6d4be48f8a3eb4.rmeta: C:\Users\alicj\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\shlex-1.3.0\src\lib.rs C:\Users\alicj\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\shlex-1.3.0\src\bytes.rs
+
+C:\Users\alicj\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\shlex-1.3.0\src\lib.rs:
+C:\Users\alicj\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\shlex-1.3.0\src\bytes.rs:
---
+++ tt_node/src/main.rs
@@ -0,0 +1,30 @@
+// src/main.rs
+#![forbid(unsafe_code)]
+
+mod core;
+mod chain_store;
+mod state_priv;
+mod randomx_full;
+
+mod falcon_sigs;
+mod kyber_kem;
+mod crypto_kmac_consensus;
+mod hybrid_commit;
+mod node_id;
+mod rtt_pro;
+mod golden_trio;
+mod consensus_weights;
+mod consensus_pro;
+mod snapshot_pro;
+mod stark_full;
+mod tx_stark;
+mod winterfell_range; // i tak jest pod cfg(feature)
+mod crypto;           // <-- tu siedzi kmac.rs i kmac_drbg.rs
+mod pqc_verification;
+
+mod p2p; // p2p/mod.rs
+
+fn main() {
+    // Na razie tylko placeholder.
+    println!("tt_node: PQC node skeleton ready.");
+}
